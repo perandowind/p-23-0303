0405c0af94f9dfbad23900af86343d9f3335eb83
perandowind <perandowind@gmail.com>
2026-03-03 14:33:42 +0900

    init

---
+++ src/main/java/com/back/AppConfig.java
@@ -0,0 +1,11 @@
+package com.back;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public
+}
---
+++ src/main/java/com/back/P230303Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P230303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P230303Application.class, args);
+	}
+
+}
