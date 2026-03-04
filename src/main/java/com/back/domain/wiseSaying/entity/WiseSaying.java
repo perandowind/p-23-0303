247adafb6a49cc26c0d7f81c257abfce493d12f3
perandowind <perandowind@gmail.com>
2026-03-04 14:35:44 +0900

    19

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -2,10 +2,12 @@ package com.back.domain.wiseSaying.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class WiseSaying {
     private int id;
     private String content;
